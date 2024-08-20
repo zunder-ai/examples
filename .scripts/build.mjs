5c23c1e217c486cc94ad95a9d577fe411062a589
regenrek <hello@regenrek.at>
2024-08-20 18:27:45 +0200

    update build mjs

--- .scripts/build.mjs
+++ .scripts/build.mjs
@@ -1,6 +1,9 @@
 import fsp from 'node:fs/promises'
 import { globby } from 'globby'
-import { resolve, dirname, join } from 'path'
+import { readPackageJSON } from 'pkg-types'
+import { join, resolve } from 'pathe'
+
+const stringify = contents => JSON.stringify(contents, null, 2)
 
 const packages = await globby([
   'examples/**/package.json',
@@ -9,51 +12,109 @@ const packages = await globby([
   '!**/.vercel',
   '!**/.output',
 ]).then(r => r.sort())
+const names = new Set()
 
 await fsp.rm('.vercel/output', { recursive: true, force: true })
+
+// Create public files
 await fsp.mkdir('.vercel/output/static', { recursive: true })
 await fsp.mkdir('.vercel/output/functions', { recursive: true })
-
-const paths = new Set()
-
 for (const config of packages) {
+  const { name } = await readPackageJSON(resolve(config))
   const output = resolve(config, '../.vercel/output')
   try {
-    await fsp.stat(output)
+    const stats = await fsp.stat(output)
+    if (!stats.isDirectory()) continue
   } catch {
     continue
   }
 
-  const relativePath = dirname(config).replace('examples/', '')
-  await fsp.cp(join(output, 'static'), join('.vercel/output/static', relativePath), { recursive: true })
+  await fsp.cp(join(output, 'static'), `.vercel/output/static/${name}`, {
+    recursive: true,
+  })
   await fsp.cp(
     join(output, 'functions/__nitro.func'),
-    join('.vercel/output/functions', `${relativePath}.func`),
-    { recursive: true }
+    `.vercel/output/functions/${name}.func`,
+    {
+      recursive: true,
+    }
   )
-  paths.add(relativePath)
+  names.add(name)
 }
 
-// Create simplified config.json with adjusted routing
+// Create middleware
+await fsp.mkdir('.vercel/output/functions/_middleware.func', {
+  recursive: true,
+})
+await fsp.writeFile(
+  '.vercel/output/functions/_middleware.func/index.js',
+  `
+const names = ${stringify([...names])}
+
+export default function middleware(req) {
+  const forced = req.url.match(/\\?force=(.*)$/)?.[1]
+  const hostname = req.headers.get('host')
+  const subdomain = forced || req.headers.get('cookie')?.match(/forced=([^;]*)(;|$)/)?.[1] || hostname.split('.').shift()
+
+  if (names.includes(subdomain)) {
+    const response = new Response()
+    const url = new URL(req.url)
+    response.headers.set('x-middleware-rewrite', '/' + subdomain + url.pathname)
+    if (forced) {
+      response.headers.set('set-cookie', \`forced=$\{forced}\`)
+    }
+    return response
+  }
+
+  return new Response(null, {
+    status: 307,
+    headers: {
+      Location: 'https://zunder.ai'
+    }
+  })
+
+}`
+)
+await fsp.writeFile(
+  '.vercel/output/functions/_middleware.func/.vc-config.json',
+  stringify({
+    runtime: 'edge',
+    entrypoint: 'index.js',
+  })
+)
 await fsp.writeFile(
   '.vercel/output/config.json',
-  JSON.stringify({
+  stringify({
     version: 3,
     routes: [
-      { handle: 'filesystem' },
-      ...[...paths].flatMap(path => [
-        {
-          src: `/${path}`,
-          dest: `/${path}`
-        },
-        {
-          src: `/${path}/(.*)`,
-          dest: `/${path}/$1`
-        }
-      ]),
+      {
+        src: '/(.*)',
+        middlewarePath: '_middleware',
+        continue: true,
+      },
+      {
+        handle: 'filesystem',
+      },
+      ...[...names].map(name => ({
+        src: `/${name}/(.*)`,
+        dest: `/${name}`,
+      })),
     ],
-  }, null, 2)
+  })
 )
 
-console.log('Successfully built zunder/examples:')
-paths.forEach(path => console.log(`  - ${path}`))
+console.log('Successfully built nuxt/examples:')
+let index = 0
+const README = await fsp.readFile('README.md', 'utf-8')
+const missingPackages = []
+for (const name of names) {
+  const treeChar = index++ === names.size - 1 ? '└─' : '├─'
+  process.stdout.write(`  ${treeChar} ${name}\n`)
+  if (!README.includes(`https://${name}.example.zunder.ai`)) {
+    missingPackages.push(name)
+  }
+}
+
+if (missingPackages.length) {
+  throw new Error(`Packages not found in README: ${missingPackages.join(', ')}`)
+}
