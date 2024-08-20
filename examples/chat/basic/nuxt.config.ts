a82fd67ace52df771c94c76af4d7a7226b2cbd3b
regenrek <hello@regenrek.at>
2024-08-20 18:04:38 +0200

    update base url

--- examples/chat/basic/nuxt.config.ts
+++ examples/chat/basic/nuxt.config.ts
@@ -9,9 +9,9 @@ export default defineNuxtConfig({
     exposeConfig: true
   },
 
-  app: {
-    baseURL: '/chat/basic/'
-  },
+  // app: {
+  //   baseURL: '/chat/basic/'
+  // },
 
   compatibilityDate: '2024-08-20'
 })
