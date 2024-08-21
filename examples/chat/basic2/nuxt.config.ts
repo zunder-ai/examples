d34bcf06c63c1ebaa45fce827bd1dac4885be83f
regenrek <hello@regenrek.at>
2024-08-21 04:46:44 +0200

    Up basic 2

---
+++ examples/chat/basic2/nuxt.config.ts
@@ -0,0 +1,21 @@
+export default defineNuxtConfig({
+  modules: ['@nuxt/ui', '@zunderai/ui'],
+  colorMode: {
+    classSuffix: '',
+  },
+  // https://tailwindcss.nuxtjs.org
+  tailwindcss: {
+    // Expose tailwind config with #tailwind-config
+    exposeConfig: true
+  },
+
+  runtimeConfig: {
+    openaiApiKey: process.env.OPENAI_API_KEY || "",
+    openaiModel: process.env.OPENAI_MODEL || "",
+    public: {
+      useSimulatedChat: process.env.USE_SIMULATED_CHAT === "true",
+    },
+  },
+
+  compatibilityDate: '2024-08-20'
+})
