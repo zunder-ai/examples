459f03f844e7f75e8b12e5694d69c0899be24e84
regenrek <hello@regenrek.at>
2024-08-21 05:01:09 +0200

    update tailwind

--- examples/chat/basic/tailwind.config.ts
+++ examples/chat/basic/tailwind.config.ts
@@ -2,14 +2,6 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
   theme: {
     extend: {
       aspectRatio: {
